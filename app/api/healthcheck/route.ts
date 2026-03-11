9a4b02e72c666b052249ea1a2ffc7143dc87d829
sumitdoescode <sumit.does.code@gmail.com>
2026-03-11 14:13:55 +0530

    remove node api

--- app/api/healthcheck/route.ts
+++ app/api/healthcheck/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function GET() {
-    return NextResponse.json({ message: "OK", timestamp: new Date().toISOString(), uptime: process.uptime() });
+    return NextResponse.json({ message: "OK", timestamp: new Date().toISOString() });
 }
