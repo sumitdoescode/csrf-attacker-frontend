8ad96cb8fa9511fe03414524b2a5ebb48796fb6c
sumitdoescode <sumit.does.code@gmail.com>
2026-03-14 13:12:47 +0530

    lajsdf

--- app/page.tsx
+++ app/page.tsx
@@ -4,48 +4,48 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const page = () => {
-    const logout = async () => {
-        try {
-            const { data } = await axios.get("https://csrf-backend.sumitdoescode.me/api/users/logout", {
-                withCredentials: true,
-            });
-            console.log(data);
-        } catch (error: any) {
-            console.log(error.response.data);
-        }
-    };
+    // const logout = async () => {
+    //     try {
+    //         const { data } = await axios.get("https://csrf-backend.sumitdoescode.me/api/users/logout", {
+    //             withCredentials: true,
+    //         });
+    //         console.log(data);
+    //     } catch (error: any) {
+    //         console.log(error.response.data);
+    //     }
+    // };
 
-    const createTodo = async () => {
-        try {
-            const { data } = await axios.post(
-                "https://csrf-backend.sumitdoescode.me/api/todos",
-                {
-                    title: "todo by hacker",
-                    description: "description of todo by hacker",
-                },
-                {
-                    withCredentials: true,
-                },
-            );
-            console.log(data);
-        } catch (error: any) {
-            console.log(error.response.data);
-        }
-    };
+    // const createTodo = async () => {
+    //     try {
+    //         const { data } = await axios.post(
+    //             "https://csrf-backend.sumitdoescode.me/api/todos",
+    //             {
+    //                 title: "todo by hacker",
+    //                 description: "description of todo by hacker",
+    //             },
+    //             {
+    //                 withCredentials: true,
+    //             },
+    //         );
+    //         console.log(data);
+    //     } catch (error: any) {
+    //         console.log(error.response.data);
+    //     }
+    // };
 
     return (
         <div>
-            <button onClick={logout} className="bg-black text-white p-2 rounded-md">
+            <a href="https://csrf-backend.sumitdoescode.me/api/users/logout" className="bg-black text-white p-2 rounded-md">
                 Logout
-            </button>
-            <button onClick={createTodo} className="bg-black text-white p-2 rounded-md">
+            </a>
+            {/* <button onClick={createTodo} className="bg-black text-white p-2 rounded-md">
                 Create Todo (via axios)
             </button>
             <form action="https://csrf-backend.sumitdoescode.me/api/todos" method="POST" encType="multipart/form-data">
                 <input type="text" hidden name="title" defaultValue="todo by hacker" />
                 <input type="text" hidden name="description" defaultValue="description of todo by hacker" />
                 <button type="submit">Create Todo (via form)</button>
-            </form>
+            </form> */}
         </div>
     );
 };
